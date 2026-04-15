a67e1b1e0263780d3b9bff37ee7a6eafcd3ad627
Dulith Thenuka <duliththenuka912@gmail.com>
2026-04-15 23:09:23 +0530

    Create Auth Controller

---
+++ src/main/java/com/example/agrolink/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.example.agrolink.controller;
+
+import com.example.agrolink.entity.User;
+import com.example.agrolink.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/register")
+    public String registerPage(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute User user) {
+        userService.register(user);
+        return "redirect:/auth/login";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+}
