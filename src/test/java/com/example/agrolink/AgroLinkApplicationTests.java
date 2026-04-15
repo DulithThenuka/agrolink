f21c3cde32c23cf8cdd6d10bd29ca9a792ed562c
Dulith Thenuka <duliththenuka912@gmail.com>
2026-04-15 22:30:28 +0530

    Initial Spring Boot AgroLink project setup

---
+++ src/test/java/com/example/agrolink/AgroLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agrolink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
