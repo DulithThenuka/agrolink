35c1746cf40925551c0c111d103fd3240cac3fa4
Dulith Thenuka <duliththenuka912@gmail.com>
2026-04-16 23:00:15 +0530

    Update User Entity (Relationship)

--- src/main/java/com/example/agrolink/entity/User.java
+++ src/main/java/com/example/agrolink/entity/User.java
@@ -1,7 +1,21 @@
 package com.example.agrolink.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
@@ -26,4 +40,7 @@ public class User {
     private Role role;
 
     private String location;
+
+    @OneToMany(mappedBy = "farmer", cascade = CascadeType.ALL)
+    private List<Crop> crops;
 }
