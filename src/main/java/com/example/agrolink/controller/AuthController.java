01b9ac07aaea8c9d4516e058b31b1273971d0756
Dulith Thenuka <duliththenuka912@gmail.com>
2026-04-21 21:35:28 +0530

    Import entity role and Remove double quotes in application properties

--- src/main/java/com/example/agrolink/controller/AuthController.java
+++ src/main/java/com/example/agrolink/controller/AuthController.java
@@ -1,10 +1,15 @@
 package com.example.agrolink.controller;
 
-import com.example.agrolink.entity.User;
-import com.example.agrolink.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.agrolink.entity.Role;
+import com.example.agrolink.entity.User;
+import com.example.agrolink.service.UserService;
 
 @Controller
 @RequestMapping("/auth")
