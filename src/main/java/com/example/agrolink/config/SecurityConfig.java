1b56bb8e8ddf179950049db240a8ffc2cf30336a
Dulith Thenuka <duliththenuka912@gmail.com>
2026-05-09 21:29:21 +0530

    Delete feature Folder because it confusing Spring Boot dependency

--- src/main/java/com/example/agrolink/config/SecurityConfig.java
+++ src/main/java/com/example/agrolink/config/SecurityConfig.java
@@ -19,6 +19,7 @@ public class SecurityConfig {
     private static final String[] PUBLIC_ENDPOINTS = {
             "/",
             "/crops",
+            "/crops/**",
             "/api/auth/**",
             "/auth/**",
             "/css/**",
