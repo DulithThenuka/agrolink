236b6841db09c5824d14fa748aa02c3b0db763f9
Dulith Thenuka <duliththenuka912@gmail.com>
2026-08-19 00:53:41 +0530

    Backend Java Compiler Error (CropController.java)

--- src/main/java/com/example/agrolink/controller/CropController.java
+++ src/main/java/com/example/agrolink/controller/CropController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.example.agrolink.dto.CropDTO;
 import com.example.agrolink.dto.CropRequestDTO;
 import com.example.agrolink.service.CropService;
 
